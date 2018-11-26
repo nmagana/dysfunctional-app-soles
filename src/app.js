5e9b484cfe305a60837f9383131ab77a79f2e42e
Nicolas Magana <nmagana2112@gmail.com>
2018-11-25 22:46:43 -0800

    Fixed footer for website

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('Welcome to React @ SOLES!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DysfunctionalApp from './components/DysfunctionalApp';
+import './styles/styles.scss'
+import 'normalize.css';
+
+ReactDOM.render(<DysfunctionalApp />, document.getElementById('app'));
--- src/model.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DysfunctionalApp from './components/DysfunctionalApp';
-
-ReactDOM.render(<DysfunctionalApp />, document.getElementById('app'));
