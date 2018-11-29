fdaecb179c06d0702b40a54fd2e8c661deb26083
Nicolas Magana <nmagana2112@gmail.com>
2018-11-29 14:23:52 -0800

    Fixed button size on narrow screen, fixed font size for widget header

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+const test = () => {
+    console.log('nice');
+}
+
 const OptionModal = (props) => (
     <Modal
         appElement={document.getElementById('app')}
         isOpen={!!props.selectedOption}
-        closeTimeoutMS={300}
+        closeTimeoutMS={200}
+        onRequestClose={test}
         className='modal'
     >
         <h1 className='modal__title'>Selected Option</h1>
-        <p className='modal__option'>{props.selectedOption}</p>
+        {props.selectedOption && <p className='modal__option'>{props.selectedOption}</p>}
         <button className='button button--exit-modal' onClick={props.handleExitModal}>Back</button>
     </Modal>
 );
